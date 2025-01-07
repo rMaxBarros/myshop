f9245dc303038838e7d514753276d46f83861f04
rMaxBarros <rmaxbarros@gmail.com>
2025-01-07 17:17:43 -0300

    Iniciando os estilos do Card.

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -4,7 +4,8 @@ import * as S from './styles';
 export const ProductCard: React.FC = () => {
     return (
         <S.Card>
-            <S.ProductImage src='' alt='' />
+            <S.ProductImage src='https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg'
+                alt='' />
 
             <S.ProductTitle>Mens Casual Premium Slim Fit T-Shirts</S.ProductTitle>
 
@@ -13,10 +14,12 @@ export const ProductCard: React.FC = () => {
                 <S.Price>$22.3</S.Price>
             </S.ReviewPriceContainer>
 
-            <S.AddToCartButton>
-                Adicionar ao carrinho
-                <FiShoppingCart />
-            </S.AddToCartButton>
+            <S.AddToCartButtonWrapper>
+                <S.AddToCartButton>
+                    Adicionar ao carrinho
+                    <FiShoppingCart />
+                </S.AddToCartButton>
+            </S.AddToCartButtonWrapper>
         </S.Card>
     )
 }
--- src/components/ProductCard/styles.ts
+++ src/components/ProductCard/styles.ts
@@ -1,15 +1,49 @@
 import styled from 'styled-components';
 
-export const Card = styled.article``;
+export const Card = styled.article`
+    background-color: white;
+    width: 100%;
+    padding: 1rem;
+    margin: 0 auto; // Centraliza a imagem
+`;
 
-export const ProductImage = styled.img``;
+export const ProductImage = styled.img`
+    width: 90%;
+`;
 
-export const ProductTitle = styled.h2``;
+export const ProductTitle = styled.h2`
+    font-weight: 500;
+    font-size: 1.2rem;
+`;
 
-export const ReviewPriceContainer = styled.div``;
+export const ReviewPriceContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
 
 export const Review = styled.span``;
 
 export const Price = styled.strong``;
 
-export const AddToCartButton = styled.button``;
+export const AddToCartButtonWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+export const AddToCartButton = styled.button`
+    border: none;
+    border-radius: 5px;
+    height: 30px;
+    padding: 0 1rem;
+    background-color: blue;
+    color: white;
+
+    display: flex;
+    align-items: center;
+    gap: 0.3rem;
+
+    svg {
+        font-size: 0.7rem;
+    }
+`;
