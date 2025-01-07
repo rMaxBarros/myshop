c4d3d0ea0fcebeeffd6d7918cb851e24e06a48e7
rMaxBarros <rmaxbarros@gmail.com>
2025-01-07 16:58:59 -0300

    Adicionando as tags da estrutura do Card

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,6 @@
+export const ProductCard: React.FC = () => {
+    return (
+        <>
+        </>
+    )
+}
---
+++ src/components/ProductCard/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Card = styled.article``;
+
+export const ProductImage = styled.img``;
+
+export const ProductTitle = styled.h2``;
+
+export const ReviewPriceContainer = styled.div``;
+
+export const Review = styled.span``;
+
+export const Price = styled.strong``;
+
+export const AddToCartButton = styled.button``;
