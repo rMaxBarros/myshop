d57ae56c1a93faec073378624dd100dec81effa3
rMaxBarros <rmaxbarros@gmail.com>
2025-01-07 15:54:49 -0300

    Aninhamento de estilos (não recomendado por motivos de padronização)

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,19 @@ import styled from 'styled-components';
 const Title = styled.h1`
   font-size: 2rem;
   color: blue;
+
+  p {
+    font-size: 1rem;
+    color: black;
+  }
 `;
 
 // Componente funcional do React. Função que retorna HTML.
 function App() {
   return (
-    <Title>Styled Components</Title>
+    <Title>Styled Components
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore sunt sit nostrum mollitia voluptate atque et consectetur facilis ut, eligendi saepe consequatur ad unde dolores dolor ea, laboriosam, rerum iure.</p>
+    </Title>
   );
 }
 
