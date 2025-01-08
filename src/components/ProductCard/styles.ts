0f0aa5382f3c65e110eedbc8986830a6078d4a36
rMaxBarros <rmaxbarros@gmail.com>
2025-01-08 16:01:29 -0300

    Estilizando as informações dos cards

--- src/components/ProductCard/styles.ts
+++ src/components/ProductCard/styles.ts
@@ -18,12 +18,17 @@ export const ProductImage = styled.img`
 export const ProductTitle = styled.h2`
     font-weight: 500;
     font-size: 1.2rem;
+
+    margin-top: 1rem;
 `;
 
 export const ReviewPriceContainer = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    width: 100%;
+
+    margin-top: 1rem;
 `;
 
 export const Review = styled.span``;
@@ -33,18 +38,23 @@ export const Price = styled.strong``;
 export const AddToCartButtonWrapper = styled.div`
     display: flex;
     justify-content: center;
+    align-items: center;
+    width: 100%;
+
+    margin-top: 1rem;
 `;
 
 export const AddToCartButton = styled.button`
     border: none;
     border-radius: 5px;
-    height: 30px;
-    padding: 0 1rem;
+    height: 40px;
+    width: 100%;
     background-color: blue;
     color: white;
 
     display: flex;
     align-items: center;
+    justify-content: center;
     gap: 0.3rem;
 
     svg {
