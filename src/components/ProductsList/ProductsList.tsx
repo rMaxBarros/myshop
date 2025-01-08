44b99d293d1e9616114902506519fce7b13c35f0
rMaxBarros <rmaxbarros@gmail.com>
2025-01-08 15:44:49 -0300

    Acessando e renderizando o produto no ProductsList

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -1,10 +1,19 @@
 import { ProductCard } from '../ProductCard/ProductCard';
+import {products} from '../../data/products';
+
 import * as S from './styles';
 
 export const ProductsList: React.FC = () => {
+    // Se preferir, pode utilizar um useEffect para puxar os dados da API de produtos.
     return (
         <S.Container>
-            <ProductCard />
+            {/* Acessando e renderizando em loop(map) os produtos vindo de products.ts 
+                Passa o produto no ProductCard como propriedade
+                Sempre que usa o map, é OBRIGATÓRIO uma propriedade key.
+            */}
+            { products.map((product) =>(
+                <ProductCard key={product.id} product={product}/>
+            ))}
         </S.Container>
     )
 }
