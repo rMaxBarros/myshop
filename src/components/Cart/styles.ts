9ea638465bad59d1e40f5a9f0face0f673099d92
rMaxBarros <rmaxbarros@gmail.com>
2025-01-08 17:17:11 -0300

    Mostrando ou Escondendo o Carrinho com styled components

--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
-export const Container = styled.aside`
+interface ContainerProps {
+    showCart: boolean;
+}
+
+export const Container = styled.aside<ContainerProps>`
     position: fixed;
     top: 0;
-    right: 0;
+    right: ${(props) => props.showCart ? '0' : '-350px'};
 
     width: 350px;
     background-color: white;
@@ -11,6 +15,8 @@ export const Container = styled.aside`
 
     padding: 2rem;
     box-shadow: -5px 0 15px rgba(0, 0, 0, 0.25);
+
+    transition: right 0.5s;
 `;
 
 export const Title = styled.h1``;
