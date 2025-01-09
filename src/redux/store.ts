8706a49060569ad653d273fa65e7fd8050fd170b
rMaxBarros <rmaxbarros@gmail.com>
2025-01-09 17:59:44 -0300

    Adicionando a biblioteca redux-logger para exibir os logs das ações do Redux.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { rootReducer } from './root-reducer';
+import logger from 'redux-logger'
 
 // Depois será substituído pelo ReduceToolKit
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(logger));
+//applyMiddleware(logger) intercepta tudo que vem do redux, sempre que um estado mudar, ele pega ela e mostra no log o que está acontecendo.
 
